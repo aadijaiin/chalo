86c2a30c6aa0070c1724940ada6b3f893cdc2979
aadijaiin <aadijain363@gmail.com>
2026-01-26 15:42:59 +0530

    removed timeout

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,7 +4,6 @@ import { toast } from "sonner";
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
-  timeout: 15000,
   headers: {
     "Content-Type": "application/json",
   },
